251aed2ec339ce79ed32a13e2ab91c09f6e0e28d
Alejandra Pinto <copinto-@student.42.us.org>
2020-05-03 22:53:11 +0200

    Course C++

---
+++ PRACTICAS3/PRAC3.1.cpp
@@ -0,0 +1,17 @@
+/*1. Escriu un programa que demani un número i em digui si és positiu o negatiu.*/
+/*ALEJANDRA PINTO*/
+/*Arkanabytes*/
+#include <iostream>
+using namespace std;
+int main(){
+    int numero;
+    cout<<"DEMANA UN NUMERO"<<endl;
+    cin>> numero;
+    
+          if(numero>0){
+              cout<<"NUMERO"<<numero<<" POSITIU";
+          }else{
+          cout<<"NUMERO"<<numero<<" NEGATIU";
+          }
+return 0;
+}
