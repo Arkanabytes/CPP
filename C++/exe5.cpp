e74e98a06df660dcde84dbd0c9b9902469d23448
Alejandra Pinto <copinto-@student.42.us.org>
2020-05-03 23:01:02 +0200

    Course C++

---
+++ C++/exe5.cpp
@@ -0,0 +1,15 @@
+/*Arkanabytes*/
+#include <iostream>
+using namespace std;
+int main (){
+	int EDAD;
+	char SEXO[10];
+	float ALTURA;
+	cout<< "DIGITE SU EDAD: "; cin>>EDAD;
+	cout<< "DIGITE SU SEXO: "; cin>>SEXO;
+	cout<< "DIGITE SU ALTURA EN METROS: "; cin>>ALTURA;
+	cout <<"\nEDAD" <<EDAD <<endl;
+	cout<< "SEXO: " <<SEXO <<endl;
+	cout << "ALTURA EN METROS: " <<ALTURA <<endl;
+	return 0;
+}
