6cbde856dbd2ee7fada4980181b34460b979a681
Alejandra Pinto <copinto-@student.42.us.org>
2020-05-03 23:05:26 +0200

    C++

---
+++ C++/exe6.cpp
@@ -0,0 +1,12 @@
+/*Arkanabytes*/
+#include <iostream>
+using namespace std;
+int main (){
+	float a,b, resultado =0;
+	cout <<"Digite el valor a: "; cin>>a;
+	cout <<"Digite el valor b: "; cin>>b;
+	resultado =(a/b)+1;
+	cout.precision(2);
+	cout <<"\nEl resultado es: " <<resultado <<endl;
+	return 0;
+}
