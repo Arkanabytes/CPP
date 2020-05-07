ac4a68edc1aca24ff435007d97d9df2e6262166b
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-05-07 19:55:42 +0200

    Course C++

--- PRACTICAS1/EXERCICIO3.cpp
+++ PRACTICAS1/EXERCICIO3.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main(){
 	
 	int num; //definiendo la variable
-	cout <<"Digite un numero: ";
-	cin>>num;//numero guardado
+	cout <<"Digite un numero: ";cin>>num;//numero guardado
 	cout<<"\nEl numero que fue digitado es: " <<num;
 	return 0;
 }
