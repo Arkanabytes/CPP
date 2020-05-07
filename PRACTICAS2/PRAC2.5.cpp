96b5d2bc26fb7bf218d20fe427a03da0ca3a21fe
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-05-07 20:01:58 +0200

    Course C++

--- PRACTICAS2/PRAC2.5.cpp
+++ PRACTICAS2/PRAC2.5.cpp
@@ -1,5 +1,5 @@
-/*Donat un string com «Java», retorna un string com «JJaJavJava». Per exemple, del
-string «Classe», hauria de retornar «CClClaClasClassClasse».*/
+/*Donat un string com Â«JavaÂ», retorna un string com Â«JJaJavJavaÂ». Per exemple, del
+string Â«ClasseÂ», hauria de retornar Â«CClClaClasClassClasseÂ».*/
 /*ALEJANDRA PINTO*/
 /*Arkanabytes*/
 #include <iostream>
@@ -8,24 +8,19 @@ using namespace std;
 
 int main (){
 	string paraula;
-	
-    cout << "introdueix paraula";
-	cin >> paraula;
+	cout << "introdueix paraula";cin >> paraula;
 
-// Trosseja la paraula a lletres
+	// Trosseja la paraula a lletres
 	paraula = "JJaJavJava";
 	int mida = paraula.length();
 	for (int i=0; i<=mida; i++){
     	cout << paraula [i] << endl;
     	i++;
-    	
-	for (int i=0; i<mida; i++){
+    	for (int i=0; i<mida; i++){
     	cout << paraula[i] << endl;
-    	i++;*/
-    	
-		for(int i=0; i<=mida; i++){
+    	i++;
+    		for(int i=0; i<=mida; i++){
 		cout << paraula.substr(0,i) << endl;
 	}
-	
 	return 0;
 }
