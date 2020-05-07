db0b611c0dde1c7750192df49cbe2b3a8c44a91b
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-05-07 19:47:44 +0200

    Course C++

--- PRACTICAS2/PRAC2.6.cpp
+++ PRACTICAS2/PRAC2.6.cpp
@@ -1,4 +1,4 @@
-/*Donat un string, retorna'n una versió sense cap vocal, a no ser que aquestes es trobin
+/*Donat un string, retorna'n una versiÃ³ sense cap vocal, a no ser que aquestes es trobin
 en un dels extrems del string. Per exemple, hola ? hla, avui ? avi, mollerussa ? mllrss*/
 /*Alejandra Pinto - Arkanabytes*/
 #include <iostream>
@@ -7,18 +7,14 @@ using namespace std;
 int main (){
 	
 	string paraula;
-	cout <<"Introdueix paraula";
-	cin>> paraula;
+	cout <<"Introdueix paraula";cin>> paraula;
 	int mida = paraula.length();
-	
-    char ch; 
-	
-    for (int i=0; i<=mida; i++){
-    	ch = paraula[i];
-	    if(ch!='a' && ch!='e'&& ch!='i' && ch!='o'&& ch!='u') {
-	    	cout << ch;
+	char ch; 
+	 for (int i=0; i<=mida; i++){
+    		ch = paraula[i];
+	  if(ch!='a' && ch!='e'&& ch!='i' && ch!='o'&& ch!='u') {
+	  	cout << ch;
 	    }    
     }
     return 0;
 }
-
