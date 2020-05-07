0d7cff6587a6197c74af3c83dc18d5f760643fcb
Alejandra Pinto <copinto-@student.42.us.org>
2020-05-07 18:13:16 +0200

    Course C++

---
+++ PRACTICAS2/PRAC2.8.cpp
@@ -0,0 +1,16 @@
+/*Donades les dos cares d'un rectangle, calcula'n l'àrea i el perímetre*/
+/*Arkanabytes-Alejandra Pinto*/
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() { 
+int base,altura; 
+  cout<<"Ingrese la base del rectangulo: "; 
+  cin>>base; 
+  cout<<"Ingrese la altura del rectangulo: "; 
+  cin>>altura; 
+  cout<<"El area del rectangulo es: "<<base*altura <<endl; 
+  cout<<"\nEl perimetro del rectangulo es: "<<base*2+altura*2 <<endl; 
+return 0;
+}
+	
