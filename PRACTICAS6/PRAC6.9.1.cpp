61fdb83b07531999d092643cd84bdf9baa655734
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-10 23:43:19 +0200

    .

--- PRACTICAS6/PRAC6.9.1.cpp
+++ PRACTICAS6/PRAC6.9.1.cpp
@@ -1,3 +1,4 @@
+/*Arkanabytes*/
 #include <iostream>
 #include <conio.h>
 #include <list>
@@ -6,7 +7,7 @@ int main ()
 {
 	llistaint1 A[10]={1789, 2035, 1899, 1456, 2013, 2016, 1458, 2458, 1254, 1472};// declaro els vectors	
     llistaint2 major=A [0], menor= A[0];//declaro els vectors major i menor
-    for (int x = 0; x <10; x++){ //mitjaçant un for 
+    for (int x = 0; x <10; x++){ //mitjaÃ§ant un for 
     	if (llistaint1 A[x]>major) major= llistaint1 A [x];
     	if (llistaint1 A[x]<menor) menor= llistaint1 A [x];}
 	cout <<"El numero mes gran es: "<<major <<endl;
